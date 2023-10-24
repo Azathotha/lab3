a9826fb242294ff391072af9f612e45a2c6f9b49
Azathotha <93633837+Azathotha@users.noreply.github.com>
2023-10-24 10:47:41 -0700

    fix bugs in ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 8, 2, 9, 4 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 4, 9, 2, 8, 3 }, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 2, 3, 4, 4, 5};
+    assertArrayEquals(new int[]{ 5, 4, 4, 3, 2 }, ArrayExamples.reversed(input1));
   }
 }
